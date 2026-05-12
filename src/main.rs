6ad22eb2c016b4702cbb27dd392a8515f4559ef7
RafaMiyauchi <rafamiyauchi@gmail.com>
2026-05-12 13:22:43 +0700

    Initial publisher code.

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!("Message received on handler 1: {:?}", message);
+        Ok(())
+    }
+    
+    // Adding the missing trait method again to avoid compiler errors!
+    fn get_handler_action(&self) -> String {
+        "".to_owned()
+    }
+}
+
+fn main() {
+    let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    
+    let _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage { user_id: "1".to_owned(), user_name: "2406453581-Amir".to_owned() });
+    let _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage { user_id: "2".to_owned(), user_name: "2406453581-Budi".to_owned() });
+    let _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage { user_id: "3".to_owned(), user_name: "2406453581-Cica".to_owned() });
+    let _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage { user_id: "4".to_owned(), user_name: "2406453581-Dira".to_owned() });
+    let _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage { user_id: "5".to_owned(), user_name: "2406453581-Emir".to_owned() });
+}
